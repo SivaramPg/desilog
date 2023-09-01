893c7638742eec03ca1088e65bc3bb21a9f3568f
SivaramPg <sivarampg95@gmail.com>
2023-09-01 14:24:46 +0530

    fix path

--- src/app/i/characters/[id]/route.ts
+++ src/app/i/characters/[id]/route.ts
@@ -19,7 +19,7 @@ export async function GET(
     })
 
     const imgBuffer = await sharp(
-      path.resolve(`./public/static/characters/${id}.png`)
+      path.resolve(`./public/static/characters/vibrant/${id}.png`)
     )
       .resize(width)
       .png({ quality: 85 })
