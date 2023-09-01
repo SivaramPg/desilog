55a7f01cf61cc23e85f158503849f076cfd17ae9
SivaramPg <sivarampg95@gmail.com>
2023-09-01 22:42:16 +0530

    add external link icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { AVATARS_COUNT } from '@/constants'
 export default function Home() {
   return (
     <main className="w-full min-h-screen py-20">
-      <h2 className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-black text-center drop-shadow-lg">
+      <h2 className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-black text-center drop-shadow-lg mb-4">
         `Dynamic` Desi Avatars
       </h2>
       <section className="max-w-screen-sm mx-auto grid grid-flow-row-dense grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 py-10 px-4">
@@ -14,19 +14,21 @@ export default function Home() {
             key={i}
             href={`https://desilog.sivaramp.com/i/avatars/${i + 1}/200`}
             target="_blank"
+            className="relative"
           >
             <AvatarImage
               className="shadow-xl drop-shadow-xl"
               url={`https://desilog.sivaramp.com/i/avatars/${i + 1}/200`}
               index={i}
             />
+            <ExternalIcon className="absolute top-[2px] right-[2px] w-3 h-3" />
           </a>
         ))}
       </section>
 
       <div className="border-b border-gray-500 my-10" />
 
-      <h2 className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-black text-center drop-shadow-lg">
+      <h2 className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-black text-center drop-shadow-lg mb-4">
         `Optimised` Desi Avatars
       </h2>
       <section className="max-w-screen-lg mx-auto grid grid-flow-row-dense grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 py-10 px-4">
@@ -37,6 +39,7 @@ export default function Home() {
               i + 1
             }.jpg`}
             target="_blank"
+            className="relative"
           >
             <AvatarImage
               className="rounded-3xl shadow-xl drop-shadow-xl"
@@ -45,13 +48,14 @@ export default function Home() {
               }.jpg`}
               index={i}
             />
+            <ExternalIcon className="absolute top-3 right-3 w-5 h-5" />
           </a>
         ))}
       </section>
 
       <div className="border-b border-gray-500 my-10" />
 
-      <h2 className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-black text-center drop-shadow-lg">
+      <h2 className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-black text-center drop-shadow-lg mb-4">
         `Raw` Desi Avatars
       </h2>
       <section className="max-w-screen-2xl mx-auto grid grid-flow-row-dense grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 py-10 px-4">
@@ -62,6 +66,7 @@ export default function Home() {
               i + 1
             }.png`}
             target="_blank"
+            className="relative"
           >
             <AvatarImage
               className="rounded shadow-xl drop-shadow-xl"
@@ -70,9 +75,25 @@ export default function Home() {
               }.png`}
               index={i}
             />
+            <ExternalIcon className="absolute top-3 right-3 w-5 h-5" />
           </a>
         ))}
       </section>
     </main>
+  )
+}
+
+const ExternalIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      className={className}
+      viewBox="0 0 512 512"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill="#000000"
+        d="M432 320h-32a16 16 0 0 0-16 16v112H64V128h144a16 16 0 0 0 16-16V80a16 16 0 0 0-16-16H48a48 48 0 0 0-48 48v352a48 48 0 0 0 48 48h352a48 48 0 0 0 48-48V336a16 16 0 0 0-16-16ZM488 0H360c-21.37 0-32.05 25.91-17 41l35.73 35.73L135 320.37a24 24 0 0 0 0 34L157.67 377a24 24 0 0 0 34 0l243.61-243.68L471 169c15 15 41 4.5 41-17V24a24 24 0 0 0-24-24Z"
+      />
+    </svg>
   )
 }
