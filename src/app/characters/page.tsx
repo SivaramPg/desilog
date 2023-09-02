e2bf25bb72bccad04df8a5ffeaae7253f4e7a393
SivaramPg <sivarampg95@gmail.com>
2023-09-02 08:14:51 +0530

    root page updates & new pages

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,27 +1,27 @@
 import { AvatarImage } from '@/app/components/AvatarImage'
 
-import { AVATARS_COUNT } from '@/constants'
+import { CHARACTERS_COUNT } from '@/constants'
 
 export default function Home() {
   return (
     <main className="w-full min-h-screen py-20">
       <h2 className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-black text-center drop-shadow-lg mb-4">
-        `Dynamic` Desi Avatars
+        `Dynamic` Desi Characters
       </h2>
-      <section className="max-w-screen-sm mx-auto grid grid-flow-row-dense grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 py-10 px-4">
-        {new Array(AVATARS_COUNT).fill(0).map((_, i) => (
+      <section className="max-w-screen-md mx-auto grid grid-flow-row-dense grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 py-10 px-4">
+        {new Array(CHARACTERS_COUNT).fill(0).map((_, i) => (
           <a
             key={i}
-            href={`https://desilog.sivaramp.com/i/avatars/${i + 1}/200`}
+            href={`https://desilog.sivaramp.com/i/characters/${i + 1}/200`}
             target="_blank"
             className="relative"
           >
             <AvatarImage
-              className="shadow-xl border hover:shadow-2xl hover:border-2 duration-200"
-              url={`https://desilog.sivaramp.com/i/avatars/${i + 1}/200`}
+              className="rounded-3xl shadow-xl border hover:shadow-2xl hover:border-2 duration-200"
+              url={`https://desilog.sivaramp.com/i/characters/${i + 1}/200`}
               index={i}
             />
-            <ExternalIcon className="absolute top-[2px] right-[2px] w-3 h-3" />
+            <ExternalIcon className="absolute top-3 right-3 w-3 h-3" />
           </a>
         ))}
       </section>
@@ -29,21 +29,21 @@ export default function Home() {
       <div className="border-b border-gray-500 my-10" />
 
       <h2 className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-black text-center drop-shadow-lg mb-4">
-        `Optimised` Desi Avatars
+        `Optimised` Desi Characters
       </h2>
       <section className="max-w-screen-lg mx-auto grid grid-flow-row-dense grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 py-10 px-4">
-        {new Array(AVATARS_COUNT).fill(0).map((_, i) => (
+        {new Array(CHARACTERS_COUNT).fill(0).map((_, i) => (
           <a
             key={i}
-            href={`https://desilog-assets.sivaramp.com/static/avatars/${
+            href={`https://desilog-assets.sivaramp.com/static/characters/vibrant/${
               i + 1
             }.jpg`}
             target="_blank"
             className="relative"
           >
             <AvatarImage
-              className="rounded-3xl shadow-xl border hover:shadow-2xl hover:border-2 duration-200"
-              url={`https://desilog-assets.sivaramp.com/static/avatars/${
+              className="rounded-xl shadow-xl border hover:shadow-2xl hover:border-2 duration-200"
+              url={`https://desilog-assets.sivaramp.com/static/characters/vibrant/${
                 i + 1
               }.jpg`}
               index={i}
@@ -56,21 +56,21 @@ export default function Home() {
       <div className="border-b border-gray-500 my-10" />
 
       <h2 className="text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-black text-center drop-shadow-lg mb-4">
-        `Raw` Desi Avatars
+        `Raw` Desi Characters
       </h2>
       <section className="max-w-screen-2xl mx-auto grid grid-flow-row-dense grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 py-10 px-4">
-        {new Array(AVATARS_COUNT).fill(0).map((_, i) => (
+        {new Array(CHARACTERS_COUNT).fill(0).map((_, i) => (
           <a
             key={i}
-            href={`https://desilog-assets.sivaramp.com/raw/avatars/${
+            href={`https://desilog-assets.sivaramp.com/raw/characters/vibrant/${
               i + 1
             }.png`}
             target="_blank"
             className="relative"
           >
             <AvatarImage
-              className="rounded shadow-xl border hover:shadow-2xl hover:border-2 duration-200"
-              url={`https://desilog-assets.sivaramp.com/raw/avatars/${
+              className="rounded object-contain shadow-xl border hover:shadow-2xl hover:border-2 duration-200"
+              url={`https://desilog-assets.sivaramp.com/raw/characters/vibrant/${
                 i + 1
               }.png`}
               index={i}
