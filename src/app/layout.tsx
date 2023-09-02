a6f436f0f5dd9f634773d34c5627fd9fc07cf594
SivaramPg <sivarampg95@gmail.com>
2023-09-02 12:26:54 +0530

    prefetch icon sprites

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Mulish } from 'next/font/google'
+import Image from 'next/image'
+import Footer from './components/Footer'
 
 const mulish = Mulish({ subsets: ['latin'] })
 
@@ -16,7 +18,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={mulish.className}>{children}</body>
+      <body className={mulish.className}>
+        <Image
+          priority
+          src={'/icons/sprite.svg'}
+          width={0}
+          height={0}
+          alt="Prefetch SVG Sprites"
+          className="hidden"
+        />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
