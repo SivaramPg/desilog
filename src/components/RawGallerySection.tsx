a46ccb7ff957a11f382c2bb06f04bfc0cb1d649a
SivaramPg <sivarampg95@gmail.com>
2023-09-04 19:28:43 +0530

    add support to switch between raw PNGs & JPEGS

--- src/components/RawGallerySection.tsx
+++ src/components/RawGallerySection.tsx
@@ -1,9 +1,13 @@
+'use client'
+
+import { useState } from 'react'
 import Link from 'next/link'
 import { twMerge as cn } from 'tailwind-merge'
 
 import { LinkImage } from './LinkImage'
 
 import { ExternalIcon } from '@/icons/ExternalIcon'
+import FileExtensionSelection from './FileExtensionSelection'
 
 interface RawGallerySectionProps {
   className?: string
@@ -20,6 +24,8 @@ const RawGallerySection = ({
   galleryItemsCount,
   galleryItemType,
 }: RawGallerySectionProps): JSX.Element => {
+  const [extension, setExtension] = useState<'jpg' | 'png'>('png')
+
   return (
     <div className={cn(className)}>
       <h2
@@ -34,14 +40,21 @@ const RawGallerySection = ({
         </Link>
         {sectionTitle}
       </h2>
+
+      <FileExtensionSelection
+        className="mt-10"
+        extension={extension}
+        setExtension={setExtension}
+      />
+
       <section className="max-w-screen-2xl mx-auto grid grid-flow-row-dense grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 py-10 px-4">
         {new Array(galleryItemsCount).fill(0).map((_, i) => (
           <LinkImage
-            key={i}
+            key={i + '.' + extension}
             className="rounded object-contain shadow-xl border hover:shadow-2xl hover:border-4 hover:border-fuchsia-500 duration-200"
             url={`https://desilog.sivaramp.com/raw/${galleryItemType}/${
               i + 1
-            }.png`}
+            }.${extension}`}
             index={i}
             customElement={
               <ExternalIcon className="absolute top-3 right-3 w-5 h-5" />
