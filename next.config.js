d66c640d58b28299d1a747fba560dd8f73e96b5b
SivaramPg <sivarampg95@gmail.com>
2023-09-05 13:30:18 +0530

    cleanup next config

--- next.config.js
+++ next.config.js
@@ -1,27 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    outputFileTracingExcludes: {
-      'app/i/avatars/[id]/route.ts': ['./src/assets/static/characters'],
-      'app/i/characters/[id]/route.ts': [
-        './src/assets/static/avatars',
-        './src/assets/static/characters/mono',
-      ],
-      'app/i/characters-bw/[id]/route.ts': [
-        './src/assets/static/avatars',
-        './src/assets/static/characters/vibrant',
-      ],
-    },
-    outputFileTracingIncludes: {
-      'app/i/avatars/[id]/route.ts': ['./src/assets/static/avatars'],
-      'app/i/characters/[id]/route.ts': [
-        './src/assets/static/characters/vibrant',
-      ],
-      'app/i/characters-bw/[id]/route.ts': [
-        './src/assets/static/characters/mono',
-      ],
-    },
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
