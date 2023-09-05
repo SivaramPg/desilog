a5f36d7d03339722bcf7fe5e074b2fa71a908682
SivaramPg <sivarampg95@gmail.com>
2023-09-05 12:40:47 +0530

    updates for optimising serverless function execution

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    outputFileTracingExcludes: {
+      'i/avatars/[id]/route.ts': ['./src/assets/static/characters'],
+      'i/characters/[id]/route.ts': [
+        './src/assets/static/avatars',
+        './src/assets/static/characters/mono',
+      ],
+      'i/characters-bw/[id]/route.ts': [
+        './src/assets/static/avatars',
+        './src/assets/static/characters/vibrant',
+      ],
+    },
+    outputFileTracingIncludes: {
+      'i/avatars/[id]/route.ts': ['./src/assets/static/avatars'],
+      'i/characters/[id]/route.ts': ['./src/assets/static/characters/vibrant'],
+      'i/characters-bw/[id]/route.ts': ['./src/assets/static/characters/mono'],
+    },
+  },
+}
 
 module.exports = nextConfig
