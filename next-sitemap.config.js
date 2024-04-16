4e747803f3a96206ddd227783540857150c47880
SivaramPg <sivarampg95@gmail.com>
2024-04-16 19:22:46 +0530

    Fix formatting issues in configuration files

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://desilog.sivaramp.com',
-  generateIndexSitemap: false,
-  exclude: ['/i*', '/r*'],
+	siteUrl: "https://desilog.sivaramp.com",
+	generateIndexSitemap: false,
+	exclude: ["/i*", "/r*"],
 }
